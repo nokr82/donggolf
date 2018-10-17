4e3dc5a5b5de2c992dc20e3adb397f950a868559
dev3 <cj920406@naver.com>
2018-10-17 19:53:51 +0900

    진선 등록

--- app/src/main/java/donggolf/android/activities/AddPostActivity.kt
+++ app/src/main/java/donggolf/android/activities/AddPostActivity.kt
@@ -1,28 +1,93 @@
 package donggolf.android.activities
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import donggolf.android.R
+import donggolf.android.actions.ContentAction
+import donggolf.android.actions.JoinAction
+import donggolf.android.base.PrefUtils
 import donggolf.android.base.RootActivity
+import donggolf.android.base.Utils
+import donggolf.android.models.Content
 import kotlinx.android.synthetic.main.activity_add_post.*
 
 class AddPostActivity : RootActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+
+    private lateinit var context: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_post)
 
+        context = this
+
+        mAuth = FirebaseAuth.getInstance()
+
         btn_finish.setOnClickListener {
             finish()
         }
-        btn_go_findpictureactivity.setOnClickListener {
+        movefindpictureBT.setOnClickListener {
             MoveFindPictureActivity()
         }
+
+        addcontentBT.setOnClickListener {
+            addContent()
+            Log.d("yjs", "BTClick")
+        }
+
     }
 
 
-    fun MoveFindPictureActivity(){
+    private fun MoveFindPictureActivity(){
         startActivity(Intent(this,FindPictureActivity::class.java))
+    }
+
+    private fun addContent(){
+
+        val title = Utils.getString(titleET)
+        if (title.isEmpty()){
+            Utils.alert(context, "제목을 입력해주세요.")
+            return
+        }
+
+        var content = Utils.getString(contentET)
+        if (content.isEmpty()){
+            Utils.alert(context, "내용을 입력해주세요.")
+            return
+        }
+
+        val user = mAuth.currentUser
+
+        if (user == null){
+            val uid = PrefUtils.getStringPreference(context, "uid")
+
+            val now = System.currentTimeMillis()
+
+            val content = Content(now,0,0,"test","incheon","제목 테스트 입니다.","테스트 입니다.",0,false,
+                    0,0, "고발자 닉네임 테스트",false,0,false)
+
+
+
+            ContentAction.saveContent(content){success: Boolean, key: String?, exception: Exception? ->
+                Log.d("yjs","key" + key)
+
+                if (success){
+
+                    Log.d("yjs", "success")
+
+                } else {
+
+                    Log.d("yjs", "excption")
+
+                }
+            }
+
+        }
     }
 }
--- app/src/main/java/donggolf/android/models/Content.kt
+++ app/src/main/java/donggolf/android/models/Content.kt
@@ -2,11 +2,22 @@ package donggolf.android.models
 
 data class Content (
 
-        var phone:String? = null,
-        var nick:String? = null,
-        var sex:String? = null,
-        var agree:Boolean = false
+        var createAt:Long? = null,
+        var updatedAt:Long? = null,
+        var updatedCnt:Int? = null,
+        var owner:String? = null,
+        var region:String? = null,
+        var title:String? = null,
+        var texts:String? = null,
+        var door_image:Int? = null,
+        var deleted:Boolean? = false,
+        var deletedAt:Long? = null,
+        var chargecnt:Int? = 0,
+        var charge_user:String? = null,
+        var heart_user:Boolean? = false,
+        var looker:Int? = 0,
+        var exclude_looker:Boolean? = false
 
-) {
-    constructor() : this(phone = "")
+){
+    constructor() : this(createAt = 0)
 }
