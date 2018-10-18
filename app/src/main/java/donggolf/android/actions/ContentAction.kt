368b3a5228d81ec26984cd747abd2b43d92f41f7
devstories <wangbum.lee@gmail.com>
2018-10-18 10:28:52 +0900

    12

--- app/src/main/java/donggolf/android/actions/ContentAction.kt
+++ app/src/main/java/donggolf/android/actions/ContentAction.kt
@@ -19,12 +19,6 @@ class ContentAction {
             }
         }
 
-        fun updateContent(key: String, params: Content, result: (success: Boolean) -> Unit) {
-            FirebaseFirestoreUtils.save("contents", key, params) {
-                result(it)
-            }
-        }
-
         fun deleteContent(key: String, params: Content, result: (success: Boolean) -> Unit) {
             FirebaseFirestoreUtils.delete("contents", key) {
                 result(it)
