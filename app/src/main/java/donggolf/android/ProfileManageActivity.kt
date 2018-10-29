5b495d0bdc9ca98bf3d67f420716aeb0cdadc00d
admin <chashin22>
2018-10-29 19:49:47 +0900

    신행 올림

---
+++ app/src/main/java/donggolf/android/ProfileManageActivity.kt
@@ -0,0 +1,12 @@
+package donggolf.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileManageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_manage)
+    }
+}
