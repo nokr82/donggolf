28c9e73aa329661a4624afc1c86cceaa3ce2b2fb
admin <chashin22>
2018-12-18 22:19:18 +0900

    신행 올림

--- app/src/main/java/donggolf/android/activities/FriendManageActivity.kt
+++ app/src/main/java/donggolf/android/activities/FriendManageActivity.kt
@@ -6,8 +6,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import com.loopj.android.http.JsonHttpResponseHandler
 import com.loopj.android.http.RequestParams
+import com.squareup.okhttp.internal.Util
+import cz.msebera.android.httpclient.Header
 import donggolf.android.R
+import donggolf.android.actions.MateAction
 import donggolf.android.adapters.FriendCategoryAdapter
 import donggolf.android.base.PrefUtils
 import donggolf.android.base.RootActivity
@@ -32,6 +36,8 @@ class FriendManageActivity : RootActivity() {
         frdMngAdapter = FriendCategoryAdapter(context, R.layout.item_friend_category_list, friendCategoryData)
         friendCategoryLV.adapter = frdMngAdapter
 
+        getCategoryList()
+
         friendCategoryLV.setOnItemClickListener { parent, view, position, id ->
             val itt = Intent(context, FriendCategoryDetailActivity::class.java)
             itt.putExtra("groupTitle", friendCategoryData.get(position).getString("title"))
@@ -45,7 +51,7 @@ class FriendManageActivity : RootActivity() {
         }
 
         blockListLL.setOnClickListener {
-            val intent = Intent(context, RequestFriendActivity::class.java)
+            val intent = Intent(context, FriendCategoryDetailActivity::class.java)
             intent.putExtra("type","block")
             startActivity(intent)
         }
@@ -53,8 +59,11 @@ class FriendManageActivity : RootActivity() {
         friendCategoryLV.setOnItemClickListener { parent, view, position, id ->
             val data = friendCategoryData.get(position)
             val category = data.getJSONObject("MateCategory")
+
             val intent = Intent(context, FriendCategoryDetailActivity::class.java)
             intent.putExtra("category_id", Utils.getInt(category,"id"))
+            intent.putExtra("category_title", Utils.getString(category,"category"))
+            println(Utils.getInt(category,"id"))
             startActivity(intent)
         }
 
@@ -80,12 +89,15 @@ class FriendManageActivity : RootActivity() {
 
             builder.setView(dialogView)
                     .setPositiveButton("확인") { dialog, id ->
-                        //println("그룹 제목 ::: $title")
-//                        var tmp = FriendCategory("", "0", true, true, true)
-//                        tmp.cateTitle = dialogView.categoryTitleET.text.toString()
-//                        println("그룹 제목 ::: ${tmp.cateTitle}")
-//                        friendCategoryData.add(tmp)
-                        frdMngAdapter.notifyDataSetChanged()
+                        addMateCategory(Utils.getString(dialogView.categoryTitleET))
+                        /*val jsonobject = null as JSONObject
+                        jsonobject.put("category", Utils.getString(dialogView.categoryTitleET))
+                        jsonobject.put("new_post","y")
+                        jsonobject.put("chat", "y")
+                        jsonobject.put("login","y")
+                        jsonobject.put("open_mate","y")
+                        friendCategoryData.add(jsonobject)
+                        frdMngAdapter.notifyDataSetChanged()*/
                     }
                     .show()
             //val alert = builder.show() //builder를 끄기 위해서는 alertDialog에 이식해줘야 함
@@ -98,14 +110,60 @@ class FriendManageActivity : RootActivity() {
 
     }
 
-    fun getMatesList() {
+    fun getCategoryList() {
         val params = RequestParams()
-        params.put("member_id", PrefUtils.getIntPreference(context, "member_id"))
+        params.put("member_id", PrefUtils.getIntPreference(context,"member_id"))
 
+        MateAction.getCategoryInfo(params,object :JsonHttpResponseHandler(){
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, response: JSONObject?) {
+                println(response)
+                val result = response!!.getString("result")
+                if (result == "ok") {
+                    val categories = response.getJSONArray("categories")
+                    friendCategoryData.clear()
+                    for (i in 0 until categories.length()) {
+                        friendCategoryData.add(categories[i] as JSONObject)
+                    }
+                    frdMngAdapter.notifyDataSetChanged()
+                }
+            }
 
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseString: String?, throwable: Throwable?) {
+                println(responseString)
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, throwable: Throwable?, errorResponse: JSONObject?) {
+                println(errorResponse)
+            }
+        })
     }
 
-    fun addMateCategory() {
+    fun addMateCategory(category_name : String) {
         val params = RequestParams()
+        params.put("member_id", PrefUtils.getIntPreference(context,"member_id"))
+        params.put("category", category_name)
+
+        MateAction.addCategory(params, object : JsonHttpResponseHandler(){
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, response: JSONObject?) {
+                println(response)
+                val result = response!!.getString("result")
+                if (result == "ok") {
+                    val newData = response.getJSONObject("categories")
+                    val newCate = newData.getJSONObject("MateCategory")
+                    friendCategoryData.add(newCate)
+                    frdMngAdapter.notifyDataSetChanged()
+                }
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, throwable: Throwable?, errorResponse: JSONObject?) {
+                println(errorResponse)
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseString: String?, throwable: Throwable?) {
+                println(responseString)
+            }
+        })
     }
+
+
 }
