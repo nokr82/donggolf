5a5ffa294335d5ca524edcd287339cb4b9c3e21d
admin <chashin22>
2018-12-26 22:09:10 +0900

    신행 올림

--- app/src/main/java/donggolf/android/activities/GoodsDetailActivity.kt
+++ app/src/main/java/donggolf/android/activities/GoodsDetailActivity.kt
@@ -4,12 +4,16 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.view.View
 import com.loopj.android.http.JsonHttpResponseHandler
 import com.loopj.android.http.RequestParams
+import com.nostra13.universalimageloader.core.ImageLoader
 import cz.msebera.android.httpclient.Header
 import donggolf.android.R
 import donggolf.android.actions.MarketAction
+import donggolf.android.base.Config
+import donggolf.android.base.PrefUtils
 import donggolf.android.base.RootActivity
 import donggolf.android.base.Utils
 import kotlinx.android.synthetic.main.activity_goods_detail.*
@@ -23,6 +27,7 @@ class GoodsDetailActivity : RootActivity() {
     var _Images: ArrayList<String> = ArrayList<String>()
     var product_id = 0
     var seller_phone = ""
+    var tmp_member_phone = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +49,13 @@ class GoodsDetailActivity : RootActivity() {
             MoveReportActivity()
         }
 
+        contact_sellerLL.setOnClickListener {
+            var myPhoneNum = PrefUtils.getStringPreference(context,"userPhone")
+            val smsMng = SmsManager.getDefault()
+            smsMng.sendTextMessage(seller_phone,null,
+                    "[동네골프]안녕하세요? 중고장터에 올라온 게시글을 보고 연락드립니다. 판매의사 있으시면 회신 바랍니다.",
+                    null, null)
+        }
 
     }
 
@@ -67,24 +79,36 @@ class GoodsDetailActivity : RootActivity() {
                 val result = response!!.getString("result")
                 if (result == "ok"){
                     val product = response.getJSONObject("product")
+                    val market = product.getJSONObject("Market")
 
-                    categoryTV.text = "[${Utils.getString(product,"form").substringBefore(" ")}형]" +
-                            "[${Utils.getString(product,"form").substringAfter(" ")}용]" +
-                            " 브랜드 > ${Utils.getString(product,"brand")}"
-                    tagTV.text = "# ${Utils.getString(product,"brand")} 정품"
-                    sale_statusTV.text = Utils.getString(product,"status")
-                    titleTV.text = Utils.getString(product,"title")
-                    writtenDateTV.text = Utils.getString(product,"created")
-                    descriptionTV.text = Utils.getString(product,"description")
-                    priceTV.text = Utils._comma(Utils.getString(product,"price"))
-                    if (Utils.getString(product,"deliv_pay") == "구매자 부담"){
+                    categoryTV.text = "[${Utils.getString(market,"form").substringBefore(" ")}형]" +
+                            "[${Utils.getString(market,"form").substringAfter(" ")}용]" +
+                            " 브랜드 > ${Utils.getString(market,"brand")}"
+                    tagTV.text = "# ${Utils.getString(market,"brand")} 정품"
+                    sale_statusTV.text = Utils.getString(market,"status")
+                    titleTV.text = Utils.getString(market,"title")
+                    writtenDateTV.text = Utils.getString(market,"created")
+                    descriptionTV.text = Utils.getString(market,"description")
+                    priceTV.text = Utils._comma(Utils.getString(market,"price"))
+                    if (Utils.getString(market,"deliv_pay") == "구매자 부담"){
                         delivPayTV.visibility = View.VISIBLE
                     } else {
                         delivPayTV.visibility = View.INVISIBLE
                     }
-                    sale_regionTV.text = Utils.getString(product,"region").substring(0,2)
-                    trade_methodTV.text = Utils.getString(product,"trade_way")
-                    seller_phone = Utils.getString(product,"phone")
+                    sale_regionTV.text = Utils.getString(market,"region").substring(0,2)
+                    trade_methodTV.text = Utils.getString(market,"trade_way")
+                    seller_phone = Utils.getString(market,"phone")
+
+
+
+                    val seller = response.getJSONObject("seller")
+                    val member = seller.getJSONObject("Member")
+                    nickTV.text = Utils.getString(member,"nick")
+
+                    val img_uri = Utils.getString(member,"profile_img")
+                    val image = Config.url + img_uri
+                    ImageLoader.getInstance().displayImage(image, profileImgIV, Utils.UILoptionsProfile)
+
                 }
             }
 
