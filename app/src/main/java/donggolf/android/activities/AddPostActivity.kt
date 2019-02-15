6b51b69032dc7f9d0790fba0aee3f69ccc788a1d
youngju <youngjucha0316@gmail.com>
2019-02-15 14:00:43 +0900

    동우등록

--- app/src/main/java/donggolf/android/activities/AddPostActivity.kt
+++ app/src/main/java/donggolf/android/activities/AddPostActivity.kt
@@ -1112,44 +1112,10 @@ class AddPostActivity : RootActivity() {
             if (addpostTV.text.equals("수정하기")){
                 val builder = AlertDialog.Builder(context)
                 builder
-                        .setMessage("글쓰기를 취소하시겠습니까 ?")
+                        .setMessage("수정하기를 취소하시겠습니까 ?")
 
-                        .setPositiveButton("유지하고 나가기", DialogInterface.OnClickListener { dialog, id ->
+                        .setPositiveButton("계속 수정하기", DialogInterface.OnClickListener { dialog, id ->
                             dialog.cancel()
-                            val title = Utils.getString(titleET)
-                            val content = Utils.getString(contentET)
-
-                            val tmpContent = TmpContent(0, member_id.toString(), title, content)
-
-                            dbManager.inserttmpcontent(tmpContent)
-
-                            if (images_path != null && images_path!!.size > 0 ) {
-                                for (i in 0 until images_path!!.size){
-                                    val imagesPath = ImagesPath(0,member_id.toString(),images_path!!.get(i),1)
-                                    println("imagesPath ${imagesPath.path}")
-                                    dbManager.insertimagespath(imagesPath)
-                                }
-                            }
-
-                            if (videoPaths != null && videoPaths.size > 0 ) {
-                                for (i in 0 until videoPaths.size){
-                                    val videoPath = ImagesPath(0,member_id.toString(),videoPaths.get(i),2)
-                                    println("videoPath ${videoPath.path}")
-                                    dbManager.insertimagespath(videoPath)
-                                }
-                            }
-
-                            if (hashtag != null && hashtag.size > 0 ) {
-                                for (i in 0 until hashtag.size){
-                                    val hastag = ImagesPath(0,member_id.toString(),hashtag.get(i),3)
-                                    println("hastag ${hastag.path}")
-                                    dbManager.insertimagespath(hastag)
-                                }
-                            }
-
-                            temp_yn = "N"
-
-                            finish()
 
                             Utils.hideKeyboard(this)
 
