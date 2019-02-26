a8c01988d3385a9af82f6c40940750238bdfda05
dev3 <cj920406@naver.com>
2019-02-26 12:10:00 +0900

    진선 올림

--- app/src/main/java/donggolf/android/activities/NoticeActivity.kt
+++ app/src/main/java/donggolf/android/activities/NoticeActivity.kt
@@ -3,6 +3,7 @@ package donggolf.android.activities
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.loopj.android.http.JsonHttpResponseHandler
 import com.loopj.android.http.RequestParams
@@ -10,10 +11,12 @@ import cz.msebera.android.httpclient.Header
 import donggolf.android.R
 import donggolf.android.actions.MarketAction
 import donggolf.android.adapters.NoticeAdapter
+import donggolf.android.adapters.ProductTypeAdaapter
 import donggolf.android.base.PrefUtils
 import donggolf.android.base.RootActivity
 import donggolf.android.base.Utils
 import kotlinx.android.synthetic.main.activity_notice.*
+import kotlinx.android.synthetic.main.dlg_market_select_option.view.*
 import org.json.JSONObject
 
 class NoticeActivity : RootActivity() {
@@ -26,6 +29,8 @@ class NoticeActivity : RootActivity() {
 
     private lateinit var noticeAdapter: NoticeAdapter
 
+    private var productData = java.util.ArrayList<JSONObject>()
+    private lateinit var productTypeAdapter: ProductTypeAdaapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +41,8 @@ class NoticeActivity : RootActivity() {
         member_id =  PrefUtils.getIntPreference(context, "member_id")
 
         noticeAdapter = NoticeAdapter(context, R.layout.item_notice, noticeData)
+        productTypeAdapter = ProductTypeAdaapter(context, R.layout.item_dlg_market_sel_op, productData)
+
 
         listviewLV.adapter = noticeAdapter
 
@@ -48,6 +55,43 @@ class NoticeActivity : RootActivity() {
         }
 
         getkeyword()
+        getCategory()
+
+        goodsinfoLL.setOnClickListener {
+            val builder = android.app.AlertDialog.Builder(context)
+            val dialogView = layoutInflater.inflate(R.layout.dlg_market_select_option, null)
+            builder.setView(dialogView)
+            val alert = builder.show()
+            dialogView.dlg_titleTV.text = "제품 종류"
+            dialogView.dlg_btn_okTV.visibility = View.VISIBLE
+            dialogView.dlg_marketLV.visibility = View.VISIBLE
+            dialogView.dlg_exitIV.visibility = View.VISIBLE
+            dialogView.dlg_titleTV.visibility = View.VISIBLE
+            dialogView.dlg_marketLV.adapter = productTypeAdapter
+            dialogView.dlg_exitIV.setOnClickListener {
+                alert.dismiss()
+            }
+
+            dialogView.dlg_marketLV.setOnItemClickListener { parent, view, position, id ->
+                for (i in 0 until productData.size){
+                    productData[position].put("isSelectedOp", false)
+                }
+
+                var json = productTypeAdapter.getItem(position)
+                var type = json.getJSONObject("ProductType")
+                val title = Utils.getString(type, "title")
+                producttypeTV.text = title
+                producttypeTV.text = "선택"
+                main_edit_search.setText(title)
+                productData[position].put("isSelectedOp", true)
+                productTypeAdapter.notifyDataSetChanged()
+                alert.dismiss()
+            }
+
+            dialogView.dlg_btn_okTV.setOnClickListener {
+                alert.dismiss()
+            }
+        }
 
     }
 
@@ -129,4 +173,37 @@ class NoticeActivity : RootActivity() {
             }
         })
     }
+
+    fun getCategory() {
+        val params = RequestParams()
+        params.put("member_id", PrefUtils.getIntPreference(context, "member_id"))
+
+        if (productData != null) {
+            productData.clear()
+        }
+
+        MarketAction.load_category(params, object : JsonHttpResponseHandler() {
+
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, response: JSONObject?) {
+                val result = response!!.getString("result")
+                if (result == "ok") {
+                    val producttype = response.getJSONArray("producttype")
+
+                    if (producttype.length() > 0 && producttype != null) {
+                        for (i in 0 until producttype.length()) {
+                            productData.add(producttype.get(i) as JSONObject)
+                            productData.get(i).put("isSelectedOp", false)
+                        }
+                    }
+
+
+                }
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseString: String?, throwable: Throwable?) {
+
+            }
+        })
+    }
+
 }
